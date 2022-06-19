6af6a3216adab0f7ab98571c8115effd957ab24e
ZeCosta <josepedroc.costa@gmail.com>
2022-06-19 15:05:58 +0100

    Versão antes de docker

--- app-server/public/javascripts/charts.js
+++ app-server/public/javascripts/charts.js
@@ -1,16 +1,13 @@
 var ctx1 = $("#downloadrecursos");
 
-const drawDRPie = (labels,sizes) => {
+const drawDRPie = (labels,sizes,colors) => {
     const data = {
     labels: labels,
     datasets: [
         {
         label: 'Dataset 1',
         data: sizes,
-        backgroundColor: [
-            goodc,
-            badc
-        ]
+        backgroundColor: colors
         }
     ]
     };
@@ -29,6 +26,76 @@ const drawDRPie = (labels,sizes) => {
         }
     };
     chart1 = new Chart(ctx1, {
+        type: "pie",
+        data: data,
+        options: options
+    });
+}
+
+
+
+var ctx2 = $("#downloadprojetos");
+const drawDPPie = (labels,sizes,colors) => {
+    const data = {
+    labels: labels,
+    datasets: [
+        {
+        label: 'Dataset 1',
+        data: sizes,
+        backgroundColor: colors
+        }
+    ]
+    };
+    var options = {
+        responsive: false,
+        legend: {
+            display: true,
+            position: "right",
+            labels: {
+                fontColor: "#333",
+                fontSize: 34
+            }
+        },
+        animation: {
+            duration: 0
+        }
+    };
+    chart1 = new Chart(ctx2, {
+        type: "pie",
+        data: data,
+        options: options
+    });
+}
+
+
+
+var ctx3 = $("#uploads");
+const drawUPPie = (labels,sizes,colors) => {
+    const data = {
+    labels: labels,
+    datasets: [
+        {
+        label: 'Dataset 1',
+        data: sizes,
+        backgroundColor: colors
+        }
+    ]
+    };
+    var options = {
+        responsive: false,
+        legend: {
+            display: true,
+            position: "right",
+            labels: {
+                fontColor: "#333",
+                fontSize: 34
+            }
+        },
+        animation: {
+            duration: 0
+        }
+    };
+    chart1 = new Chart(ctx3, {
         type: "pie",
         data: data,
         options: options
